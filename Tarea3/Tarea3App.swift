f907fa9521cd755fca9c97e388d89d0b0e1915fd
Alfonso <90729891+AlfonsoEstudiante@users.noreply.github.com>
2025-09-15 17:11:26 -0600

    Add files via upload

---
+++ Tarea3/Tarea3App.swift
@@ -0,0 +1,17 @@
+//
+//  Tarea3App.swift
+//  Tarea3
+//
+//  Created by Alfonso Madero on 15/09/25.
+//
+
+import SwiftUI
+
+@main
+struct Tarea3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
